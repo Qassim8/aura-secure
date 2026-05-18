87a5bea797afa23160effbe0d99d658b6ed31276
qassim8 <mohammedalgasim123@gmail.com>
2026-05-18 22:27:58 +0200

    fixed ltr dir and some mobile screen size in services page

--- src/components/services/ServicesCTAWithMap.jsx
+++ src/components/services/ServicesCTAWithMap.jsx
@@ -10,9 +10,9 @@ export default function ServicesCTA() {
   const isRtl = locale === "ar";
 
   return (
-    <section className="py-16 bg-white" dir={isRtl ? "rtl" : "ltr"}>
+    <section className="py-16 bg-white">
       <div className="container mx-auto px-6">
-        <div className="relative bg-gray-50 border border-gray-100/80 p-8 md:p-16 overflow-hidden select-none">
+        <div className="relative bg-gray-50 border border-gray-100/80 p-5 md:p-16 overflow-hidden select-none">
           <div className="absolute -right-20 -top-20 w-80 h-80 bg-(--main-color)/25 rounded-full blur-3xl pointer-events-none"></div>
           <div className="absolute -left-20 -bottom-20 w-80 h-80 bg-(--primary-color)/25 rounded-full blur-3xl pointer-events-none"></div>
 
@@ -28,7 +28,7 @@ export default function ServicesCTA() {
             <div className="pt-4">
               <Link
                 href={`/${locale}/contact`}
-                className="inline-flex items-center gap-2 bg-(--primary-color) hover:bg-(--main-color) text-white px-8 py-4 font-black text-xs md:text-sm transition-all duration-300 tracking-wide cursor-pointer group shadow-md shadow-(--primary-color)/10"
+                className="inline-flex items-center gap-2 bg-(--primary-color) hover:bg-(--main-color) text-white px-4 md:px-8 py-4 font-black text-xs md:text-sm transition-all duration-300 tracking-wide cursor-pointer group shadow-md shadow-(--primary-color)/10"
               >
                 <span>{t("map_section_btn")}</span>
                 <MdOutlineArrowOutward className="text-base group-hover:rotate-45 transition-transform duration-300 text-(--main-color) group-hover:text-white" />
